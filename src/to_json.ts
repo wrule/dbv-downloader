7bf2af1823590a591060eb4a737a3fec3d09b4bc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-06 17:38:56 +0800

    提交代码

--- src/to_json.ts
+++ src/to_json.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
-function get_lines(pathname = 'download/result.csv') {
-  const csv_text = fs.readFileSync(pathname, 'utf8');
-  return csv_text
+function get_lines() {
+  const csv_text = fs.readFileSync('download/result.csv', 'utf8');
+  const lines = csv_text
     .split('\n')
     .map((line) => line.trim())
     .filter((line) => line)
@@ -10,11 +10,13 @@ function get_lines(pathname = 'download/result.csv') {
     .map((line) => line.split(','))
     .map((line) => line.map((num) => Number(num)))
     .map((line) => line.slice(0, 6));
+  lines.sort((a, b) => a[0] - b[0]);
+  return lines;
 }
 
 function main() {
   const lines = get_lines();
-  console.log(lines[0]);
+  fs.writeFileSync('download/result.json', JSON.stringify(lines, null, 2), 'utf8');
 }
 
 main();
